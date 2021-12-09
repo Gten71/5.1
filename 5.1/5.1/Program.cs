0575e5e8ebb3799a661f16f0aeea47ba80495c54
Gten71 <83307844+Gten71@users.noreply.github.com>
2021-12-09 19:35:21 +0200

    Add files via upload

---
+++ 5.1/5.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _5._1
+{
+    //средний 5.1 13 заданиен 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 126, 548, 90, 0, 21, 4, 9, 6 };
+            Array.Sort(arr);
+            Array.ForEach(arr, Console.WriteLine);
+        }
+    }
+}
